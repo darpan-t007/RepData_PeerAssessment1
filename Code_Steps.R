d2fda5390971032e6f9ca30216aa3dc6aad27a57
darpan-t007 <darpan.theng@learner.manipal.edu>
2023-04-04 21:49:46 +0530

    Created Code_Steps file and loaded the data in terminal and started the .R file for further code

---
+++ Code_Steps.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+total.steps <- tapply(data$steps, data$date, FUN=sum, na.rm=TRUE)
+qplot(total.steps, binwidth=1000, xlab="total number of steps taken each day")
+mean(total.steps, na.rm=TRUE)
+median(total.steps, na.rm=TRUE)
+
+
+## ------------------------------------------------------------------------
+library(ggplot2)
+averages <- aggregate(x=list(steps=data$steps), by=list(interval=data$interval),
+                      FUN=mean, na.rm=TRUE)
+ggplot(data=averages, aes(x=interval, y=steps)) +
+  geom_line() +
+  xlab("5-minute interval") +
+  ylab("average number of steps taken")
+
